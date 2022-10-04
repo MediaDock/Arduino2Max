2efa4b8c8757e9b277ad1358267d19b42ffb0657
MediaDock <50368920+MediaDock@users.noreply.github.com>
2022-10-04 14:40:48 +0200

    Add files via upload

---
+++ 3_Arduino2Max_Rotaryencoder.ino
@@ -0,0 +1,59 @@
+ /*
+  Rotary Encoder Demo
+  rot-encode-demo.ino
+  Demonstrates operation of Rotary Encoder
+  Displays results on Serial Monitor
+  DroneBot Workshop 2019
+  https://dronebotworkshop.com
+*/
+ 
+ // Rotary Encoder Inputs
+ #define inputCLK 4
+ #define inputDT 5
+
+
+ int counter = 1; 
+ int currentStateCLK;
+ int previousStateCLK; 
+
+ String encdir ="";
+
+ void setup() { 
+   
+   // Set encoder pins as inputs  
+   pinMode (inputCLK,INPUT);
+   pinMode (inputDT,INPUT);
+   
+   
+   // Setup Serial Monitor
+   Serial.begin (115200);
+   
+   // Read the initial state of inputCLK
+   // Assign to previousStateCLK variable
+   previousStateCLK = digitalRead(inputCLK);
+
+ } 
+
+ void loop() { 
+  
+  // Read the current state of inputCLK
+   currentStateCLK = digitalRead(inputCLK);
+    
+   // If the previous and the current state of the inputCLK are different then a pulse has occured
+   if (currentStateCLK != previousStateCLK){ 
+       
+     // If the inputDT state is different than the inputCLK state then 
+     // the encoder is rotating counterclockwise
+     if (digitalRead(inputDT) != currentStateCLK) { 
+       counter --;
+       
+     } else {
+       // Encoder is rotating clockwise
+       counter ++;
+       
+     }
+   } 
+  Serial.println(counter);
+   // Update previousStateCLK with the current state
+   previousStateCLK = currentStateCLK; 
+ }
